3c3f2e1bba259bad4c379aeb776ed9e3a335069b
Sherin Binu <sherinbinu@hotmail.com>
2019-01-10 13:23:11 +0530

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <nuxt/>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+:root {
+  --bg-one: #4c5c96;
+  --bg-two: #45558d;
+  --bg-three: #3f4e85;
+  --bg-four: #2e3141;
+}
+</style>
