46ae8554f33b81c22c7da41338c2d6f09dd7efd6
Sherin Binu <sherinbinu@hotmail.com>
2019-01-10 16:12:13 +0530

    complete hero section

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,8 +6,7 @@
 
 <style>
 html {
-  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
-    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: Raleway, sans-serif;
   font-size: 16px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
@@ -15,6 +14,7 @@ html {
   -moz-osx-font-smoothing: grayscale;
   -webkit-font-smoothing: antialiased;
   box-sizing: border-box;
+  color: white;
 }
 
 *,
@@ -25,9 +25,11 @@ html {
 }
 
 :root {
-  --bg-one: #4c5c96;
-  --bg-two: #45558d;
-  --bg-three: #3f4e85;
-  --bg-four: #2e3141;
+  --primary: #2e3141;
+  --primary-trans: #2e3141cc;
+  --accent-one: #4c5c96;
+  --accent-two: #45558d;
+  --accent-three: #3f4e85;
+  --gray: #999;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>test</h1>
+  <div class="home">
+    <div class="home__hero--container">
+      <div class="home__hero">
+        <div class="home__hero--logo">
+          <img src="/images/logo.png" alt="Digital Nomad Turkey Logo">
+        </div>
+        <div class="home__hero--title">
+          <h1>digital nomad turkey</h1>
+        </div>
+        <div class="home__hero--subtitle">
+          <p>nullam ultrices. neque et faucibus viverba</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +21,56 @@ export default {};
 </script>
 
 <style>
+.home__hero--container {
+  height: 100vh;
+  width: 100vw;
+  background: url(/images/banner-desktop.jpg) center no-repeat;
+  background-size: cover;
+  clip-path: polygon(0 0, 100% 0, 100% 90%, 10% 100%, 0% 100%);
+}
+
+.home__hero--container:after {
+  content: "";
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 100vw;
+  height: 100vh;
+  background: var(--primary-trans);
+  clip-path: polygon(0 0, 100% 0, 100% 90%, 10% 100%, 0% 100%);
+}
+
+.home__hero {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  height: 23vh;
+  width: 60vw;
+  z-index: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+.home__hero--logo img {
+  height: 4rem;
+}
+
+.home__hero--title {
+  border-bottom: 1px solid var(--gray);
+  padding-bottom: 1.5rem;
+}
+
+.home__hero--title h1 {
+  text-transform: uppercase;
+  font-weight: 700;
+  letter-spacing: 0.3rem;
+}
+
+.home__hero--subtitle p {
+  text-transform: uppercase;
+  color: var(--gray);
+  letter-spacing: 0.1rem;
+}
 </style>
